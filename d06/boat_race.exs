d928b9fd768d6edc39d4d91f0422803e0ba64a22
James Groves <james.groves@shopify.com>
2023-12-06 11:07:36 +0000

    d6-cleanup

--- d06/boat_race.exs
+++ d06/boat_race.exs
@@ -1,9 +1,11 @@
 defmodule BoatRace do
-  @input "sample.txt"
+  @input "input.txt"
 
   def part2(),
     do:
-      read_all()
+      read_input()
+      |> Enum.map(&Enum.join/1)
+      |> Enum.map(&String.to_integer/1)
       |> List.to_tuple()
       |> win_race
       |> Enum.reduce(0, fn _tup, acc -> acc + 1 end)
@@ -11,12 +13,15 @@ defmodule BoatRace do
   def part1(),
     do:
       read_input()
+      |> List.flatten()
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.chunk_every(chunk_for(@input))
       |> group
-      |> find_all_winners
+      |> find_all
       |> Enum.map(&Enum.count/1)
       |> Enum.product()
 
-  def find_all_winners(list, results \\ []) do
+  defp find_all(list, results \\ []) do
     [h | t] = list
 
     results = [win_race(h) | results]
@@ -24,52 +29,40 @@ defmodule BoatRace do
     find_winners(t, results)
   end
 
-  def find_winners([], results), do: results
+  defp find_winners([], results), do: results
 
-  def find_winners(list, results) do
+  defp find_winners(list, results) do
     [h | t] = list
 
     results = [win_race(h) | results]
     find_winners(t, results)
   end
 
-  def win_race({time, current_record_distance}) do
-    for option <- 0..time,
-        option * (time - option) > current_record_distance,
-        do: {option, option * (time - option)}
+  defp win_race({time, current_record_distance}) do
+    for hold_time <- 0..time,
+        distance = hold_time * (time - hold_time),
+        distance > current_record_distance,
+        do: {hold_time, distance}
   end
 
-  def group([times, distances]) do
+  defp group([times, distances]) do
     Enum.zip(times, distances)
   end
 
-  def chunk_for(input) do
+  defp chunk_for(input) do
     case input do
       "sample.txt" -> 3
       "input.txt" -> 4
     end
   end
 
-  def read_input,
+  defp read_input,
     do:
       @input
       |> File.read!()
       |> String.split("\n", trim: true)
       |> Enum.map(&Regex.scan(~r/\d+/, &1))
       |> Enum.map(&List.flatten/1)
-      |> List.flatten()
-      |> Enum.map(&String.to_integer/1)
-      |> Enum.chunk_every(chunk_for(@input))
-
-  def read_all(),
-    do:
-      @input
-      |> File.read!()
-      |> String.split("\n", trim: true)
-      |> Enum.map(&Regex.scan(~r/\d+/, &1))
-      |> Enum.map(&List.flatten/1)
-      |> Enum.map(&Enum.join/1)
-      |> Enum.map(&String.to_integer/1)
 end
 
 IO.inspect(BoatRace.part1(), label: "Part 1")
